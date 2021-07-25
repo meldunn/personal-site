f47665840d61dd4be0cbe7c448d133f7514ebf5b
meldunn <dmelissa216@gmail.com>
2021-07-25 18:01:03 -0400

    Add Format and Content

---
+++ personal-site/pages/index.js
@@ -0,0 +1,81 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      {/* <section className={utilStyles.center}>
+        <body>
+          <div className={utilStyles.center}>
+             <Link href="./Melissa_Dunn_CV.pdf">
+              <a>CV</a>
+            </Link> 
+     
+          </div>
+        </body>
+      </section> */}
+
+      <section className={utilStyles.headingMd}>
+        <p>
+          I am a final year Computer Science, Psychology and Mathematics student
+          at McGill University, particularly interested in Distributed Systems,
+          Concurrency, and Databases.{" "}
+        </p>
+        <p>
+          {" "}
+          Currently a Software Developer Intern at{" "}
+          <a href="https://www.morganstanley.com/"> Morgan Stanley</a>, Full
+          Stack Developer at <a href="https://mchacks.ca/">HackMcGill</a>,
+          President of the
+          <a href="https://mcgill-csus.ca/">
+            {" "}
+            McGill Computer Science Undergraduate Society
+          </a>
+          , and a CS Tutor at the{" "}
+          <a href="https://mcgill-csus.ca/helpdesk"> CSUS Helpdesk</a>.
+        </p>
+      </section>
+      <section className={utilStyles.center}>
+        <body>
+          <div className={utilStyles.center}>
+            {/* <Link href="./Melissa_Dunn_CV.pdf">
+              <a>CV</a>
+            </Link> */}
+            <Link href="https://github.com/meldunn">
+              <a>
+                <Image
+                  priority
+                  src="/images/github_icon.png"
+                 // className={utilStyles.borderCircle}
+                  height={30}
+                  width={30}
+                  alt="github"
+                />
+              </a>
+            </Link>
+            <Image priority
+                  src="/gweg" height={30} width={30} />
+            <Link href="https://www.linkedin.com/in/melissa-dunn-ba33281a4/">
+              <a>
+                <Image
+                  priority
+                  src="/images/linkedin_icon.png"
+                  //className={utilStyles.borderCircle}
+                  height={30}
+                  width={30}
+                  alt="github"
+                />
+              </a>
+            </Link>
+          </div>
+        </body>
+      </section>
+    </Layout>
+  );
+}
